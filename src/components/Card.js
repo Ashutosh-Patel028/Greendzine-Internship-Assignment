325dd174d5a2bb0d3ee8ba63adf5bbcd109d9896
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-24 17:08:19 +0530

    added badge and card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import "./Card.css";
 
-function Card(){
-
+function Card(props){
+    const {id,firstName,avatar}=props;
+    console.log(id,firstName,avatar);
+    return (
+        <div className='card-container' id={id}>
+            <img src={avatar}/>
+        </div>
+    )
 }
 
 export default Card;
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,21 +1,27 @@
 import React,{useState,useEffect} from "react";
-
-
-async function API_CALL(apiURL){
-    const response=await fetch(apiURL);
-    const jsonDATA=await response.json();
-    console.log(jsonDATA);
-}
-
+import Card from "./Card.js";
+import "./ListView.css"
 
 function ListView(){
     const apiURL=process.env.REACT_APP_API_URL;
+    const [data,setData]=useState([]);
+    
+    async function make_API_Call(apiURL){
+        const response=await fetch(apiURL);
+        const jsonDATA=await response.json();
+        // console.log(jsonDATA);
+        setData(jsonDATA.data);
+    }
     useEffect(()=>{
-        API_CALL(apiURL)
+        make_API_Call(apiURL)
     },[]);
+
+    // console.log(data[0]);
     return (
-        <div>
-            this is listview
+        <div className="list-container">
+            {data.map((val,ind)=>{
+                return <Card key={ind} id={val?.id} firstName={val?.first_name} avatar={val?.avatar}/>
+            })}
         </div>
     )
 }
